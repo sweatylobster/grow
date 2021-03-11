89a65b2a5926c607ff0632f5aace0d27998116cf
Max de Hoyos <max.dehoyos@gmail.com>
2021-03-11 05:28:32 -0800

    streamlined

--- loc/fac.py
+++ loc/fac.py
@@ -1,19 +1,9 @@
 from functools import reduce
-# number of lights:    n  (integer)
-# dimensions of light: s  (square feet)
-# size of aisle:       a  (feet)
-# ::: n*s*a  = area of facility, as 'A'.
-# A * height = volume of facility (cubic feet).
-# CFM = (V : hour :: 1.5) -- set arbitrarily and without criteria.
-# CFH must be balanced so as not to waste CO2.
-# V holds a quantity of air in mol
-# density * V = mol(air)
 
-# inferences from Chandra's "Table 2"
-# 350 umol/mol = ambient CO2/air
-# 750 umol/mol shows 50%|111%|115% increases in Pn, WUE, Ci when the two are compared.
-# 650 umol/mol 
-# ::: data suggest higher yield with (even) higher CO2, perhaps 1000 umol/mol.
+def gain(ppp=2000, ppc=20, cpy=56//12):
+    # income per year
+    ipy = ppp * ppc * cpy
+    return f"at {cpy} (complete) cycles/year, producing {ppc}lbs each, at ${ppp} a pound: ${ipy}"
 
 def elec_price(n, W, hrs):
     kW = n * W * 0.001
@@ -25,24 +15,20 @@ def elec_price(n, W, hrs):
     return ppy
     #return f'kWh costs of {n}, {W}W lights a year: ${ppy}'
 
+def soil_price(price, volume, plants):
+    total = price * plants * volume
+    return total
+
 lights = (72, 480, 15)
 cypress = (20, 725, 15)
-T4 = {'cfm': 205, 'W' : 28}
 
 # price per month
 container_year = 165 * 12
 total = reduce(lambda x, y: x+y, [elec_price(*cypress), container_year])
 
 print(f"rental of 40x8.5x8 shipping container: ${container_year}/year")
-print("environment (Temp, RH, VPD) costs unknown.")
+print("environment (soil, temp, RH) costs unknown.")
 print(f"for {cypress[0]}, {cypress[1]}W lights running {cypress[2]}h/day, ${elec_price(*cypress)}")
 print(f"total yearly cost = ${total}")
-
-
-def gain(ppp=2000, ppc=20, cpy=56//12):
-    # income per year
-    ipy = ppp * ppc * cpy
-    return f"at {cpy} (complete) cycles/year, producing {ppc}lbs each, at ${ppp} a pound: ${ipy}"
-
 print("on the other hand:")
 print(gain())
